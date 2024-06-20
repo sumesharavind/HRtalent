6adb5f2d671c36146e1b8dbac61efba80ae545e7
unknown <sumesharavind940@gmail.com>
2024-06-20 13:06:20 +0530

    All files added

---
+++ src/components/AppFooter.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { CFooter } from '@coreui/react'
+
+const AppFooter = () => {
+  return (
+    <CFooter className="px-4">
+      <div style={{justifyContent:"center"}}>
+        {/* <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">
+          CoreUI
+        </a> */}<span>All rights Reserved</span>
+        <span className="ms-1">&copy; 2024 Sumesh.</span>
+      </div>
+      {/* <div className="ms-auto">
+        <span className="me-1">Powered by</span>
+        <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">
+          CoreUI React Admin &amp; Dashboard Template
+        </a>
+      </div> */}
+    </CFooter>
+  )
+}
+
+export default React.memo(AppFooter)
